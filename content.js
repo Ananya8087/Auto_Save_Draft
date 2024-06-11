fba166dee7342c82d572f79c0e318f49df86ec6a
Ananya V <90900377+Ananya8087@users.noreply.github.com>
2024-06-11 10:47:25 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,30 +1,18 @@
 (function() {
-  // Function to click on the "Save Draft" button
+  // Function to click on the "Save as Draft" button
   function clickSaveDraftButton() {
     const saveDraftButton = document.querySelector('.qcButtons .qc_reject_btn');
     if (saveDraftButton) {
       saveDraftButton.click();
-      console.log('Clicked on the "Save Draft" button.');
+      console.log('Clicked on the "Save as Draft" button.');
     } else {
       console.log('Save Draft button not found.');
     }
   }
 
-  // Listen for clicks on "Next Image" button
-  const nextImageButton = document.querySelector('.p-button.img_control_btn img[src*="rightArrow"]');
-  if (nextImageButton) {
-    nextImageButton.closest('.p-button').addEventListener('click', () => {
-      console.log('Next Image button clicked.');
-      clickSaveDraftButton();
-    });
-  }
-
-  // Listen for clicks on "Previous Image" button
-  const prevImageButton = document.querySelector('.p-button.img_control_btn img[src*="leftArrow"]');
-  if (prevImageButton) {
-    prevImageButton.closest('.p-button').addEventListener('click', () => {
-      console.log('Previous Image button clicked.');
-      clickSaveDraftButton();
-    });
-  }
+  // Auto-click the "Save as Draft" button every 3 seconds
+  setInterval(() => {
+    console.log('Attempting to click the "Save as Draft" button.');
+    clickSaveDraftButton();
+  }, 30000); // 3000 milliseconds = 3 seconds
 })();
